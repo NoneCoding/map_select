e5d0c1f9f244df933f4a25256d94ba4db9351db4
NoneCoding <danieldebrito2105@gmail.com>
2022-08-06 15:43:22 -0300

    Moved commands/not working

--- map_select.py
+++ map_select.py
@@ -1,16 +1,8 @@
 import nextcord
 from nextcord.ext import commands
-from random import choice
 import os
 from dotenv import load_dotenv
-
-
-    
-    
-supported_games = ["csgo", "cs:go"]
-csgo_active_duty = ["Inferno", "Mirage", "Nuke", "Overpass", "Dust II", "Vertigo", "Ancient"]
-csgo_reserve = ["Train", "Cache", "Grind", "Mocha"]
-csgo_hostage = ["Militia", "Agency", "Office", "Italy", "Assault"]
+from map_command import Map
 
 load_dotenv("map_key.env")
 
@@ -20,37 +12,10 @@ intents = nextcord.Intents.all()
 
 bot = commands.Bot(prefix=".", intents=intents)
 
-    
-@bot.command()
-async def choose(self, ctx, game, *, mode="all"):
-    game = game.lower()
-        
-    if game not in Map.supported_games:
-            return await ctx.send("Esse jogo não é suportado!")
-        
-    if game in ["csgo", "cs:go"]:
-        if mode == "active duty":
-            return await ctx.send("Seu mapa é: {}".format(choice(Map.csgo_active_duty)))
-            
-    elif mode == "reserve":
-        return await ctx.send("Seu mapa é: {}".format(choice(Map.csgo_reserve)))
-            
-    elif mode == "hostage":
-        return await ctx.send("Seu mapa é: {}".format(choice(Map.csgo_hostage)))
-            
-    elif mode == "all":
-        all = []
-        all.extend(Map.csgo_active_duty)
-        all.extend(Map.csgo_reserve)
-        all.extend(Map.csgo_hostage)
-        return await ctx.send("Seu mapa é: {}".format(choice(all)))
-            
-    await ctx.send("Esse modo de jogo não é suportado :(")
-        
+bot.add_cog(Map(bot))
 
 @bot.event
 async def on_ready():
     print("Ready! Logged as {}".format(bot.user))
-    
 
 bot.run(MAP_KEY)
